51830e3352467350f1cff2912363ab66e8817236
goodluckjw <chetera@naver.com>
2025-04-16 17:47:54 +0900

    1

---
+++ app/law_editor_app.py
@@ -0,0 +1,6 @@
+
+import streamlit as st
+from utils.xml_parser import parse_law_xml
+
+st.title("부칙 개정 도우미")
+st.write("법령 본문 중 검색어를 포함하는 조문을 찾아줍니다.")
---
+++ utils/xml_parser.py
@@ -0,0 +1,4 @@
+
+def parse_law_xml(xml_string):
+    # Dummy parser for test
+    return "Parsed content"
